f2906d5bed0872e0a5910d48e67058f9da1dcc89
batuhan <batuhanyndny@gmail.com>
2019-08-07 23:24:11 +0300

    major updates on game mechanics

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 
 class Char:
 
@@ -7,45 +7,61 @@ class Char:
     damage = ''
     details = ''
     is216 = False
-    hp = 100
-    armor = 69
-    assholeArmor = 50
+    hp = 420
+    assholeArmor = 69
     assholeSize = 1
-    dickSize = 5
+    dickSize = 7
+
+    hasBlock = False
+
+    defaultDickSize = 7
+    defaultHP = 420
+    
 
     def __init__(self,title, damage, details):
         self.title = title
         self.damage = damage
         self.details = details
 
-    def move(self, Moves):
-        return self.dickSize
     
     def attack(self,Char, damage):
-        Char.hp -= damage
+        if Char.hasBlock:
+            print("NAH MAN YOUR ATTACK JUST GET BLOCKED")
+        else:
+            if Char.assholeArmor != 0:
+                if damage > Char.assholeArmor:
+                    print("PRO GAMER ATTACKK !! {0}'s ASSHOLE ARMOR JUST BROKE !! PREPARE FOR RAPE".format(Char.title))
+                    Char.hp -= damage-Char.assholeArmor
+                    Char.assholeArmor = 0
+                elif damage < Char.assholeArmor:
+                    print("ARMOR HIT !! {0}'s asshole protector damaged by {1}".format(Char.title,damage))
+                    Char.assholeArmor -= damage
+            else:
+                print("Hit !! {0}'s health decrased by {1}".format(Char.title, damage))
+                Char.hp -= damage
 
 class Moves:
 
     def amguard(Char):
         Char.assholeArmor += 20
         print('{0} HAS GAINED 20 ASSHOLE ARMOR'.format(Char.title))
-        return 1
+        time.sleep(2)
+
 
     def quadroDick(Char):
-        Char.dickSize += 20
-        print("{0}'s dick size increased by 20 cm NOW HAS x4 DAMAGE".format(Char.title))
-        return 1
+        Char.dickSize += Char.dickSize*4
+        print("{0}'s dick size increased by {1} cm NOW HAS x4 DAMAGE".format(Char.title, Char.dickSize*4))
+        time.sleep(2)
 
     def sedativeDick(Char):
         Char.dickSize = 2
         print("{0}'s dick size decrased to 2 cm DAMN NIGGA MICROPENIS".format(Char.title))
-        return 1
+        time.sleep(2)
 
     def geciktirici(Char):
-        Char.dickSize += 8
-        print("{0} used geciktirici, he attack powered and hits twice")
-        return 2
-
+        Char.dickSize += 2
+        print("{0} used geciktirici, his attack powered and hits twice")
+        time.sleep(2)
 
 
 
@@ -66,3 +82,29 @@ def currentCharList():
 #     else:
 #         sp = 1
 #         return fp, sp
+
+def countDown():
+    print("3")
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    print('Match Begins')
+    time.sleep(1)
+
+
+# i copied this func from stackoverflow -> https://stackoverflow.com/questions/48035367/python-text-game-health-bar
+def do_health(health, maxHealth):
+    healthDashes=20
+    dashConvert = int(maxHealth/healthDashes)                         # Get the number to divide by to convert health to dashes (being 10)
+    currentDashes = int(health/dashConvert)                           # Convert health to dash count: 80/10 => 8 dashes
+    remainingHealth = healthDashes - currentDashes                    # Get the health remaining to fill as space => 12 spaces
+
+    healthDisplay = ''.join(['-' for i in range(currentDashes)])      # Convert 8 to 8 dashes as a string:   "--------"
+    remainingDisplay = ''.join([' ' for i in range(remainingHealth)]) # Convert 12 to 12 spaces as a string: "            "
+    percent = str(int((health/maxHealth)*100)) + "%"                  # Get the percent as a whole number:   40%
+    
+    bar = "|{0}{1}|".format(healthDisplay, remainingDisplay)               # Print out textbased healthbar
+
+    return bar, percent
--- main.py
+++ main.py
@@ -27,29 +27,76 @@ def main():
     print('{0} plays as {1}'.format(firstPname, firstP.title))
     time.sleep(1)
     print('{0} plays as {1}'.format(secondPname, secondP.title))
+    time.sleep(1)
 
-    print("MATCH BEGINS")
-    time.sleep(2)
+    attacker, defencer = setTurnOrder(firstP, secondP)
+
+    Attacker = enumerate(attacker)
+    Defencer = enumerate(defencer)
+
+    countDown()
 
     while firstP.hp > 0 or secondP.hp > 0:
 
-        print("--{0}'s TURN--".format(firstPname))
-        print("HEALTH STATUS: \n {0}'s health is {1} \n{2}'s health is {3}".format(firstPname, firstP.hp,secondPname, secondP.hp))
+        attacker = Attacker.__next__()[1]
+
+        defencer = Defencer.__next__()[1]
+        print('\n')
+        turnDef(attacker)
+        print('\n')
+        time.sleep(1)
+        print('\n')
+        healthStatus(firstP, secondP)
 
         time.sleep(2)
+
+        print('\n')
 
         print("AVALIABLE MOVES : \namguard --1 \nquadroDick --2 \nsedativeDick --3 \ngeciktirici --4")
-        move = input("Select by typing your moves number")
         
+        print('\n')
+
+        move = input("Select by typing your moves number ->  ")
+        
+        if move == "1":
+            Moves.amguard(attacker)
+
         if move == '2':
-            ds = firstP.move(Moves.quadroDick(firstP))
-            firstP.attack(secondP, ds)
+            Moves.quadroDick(attacker)
+            attacker.attack(defencer, attacker.dickSize)
+            
+
+        if move == '3':
+            Moves.sedativeDick(defencer)
+
+        if move == '4':
+            Moves.geciktirici(attacker)
+            attacker.attack(defencer, attacker.dickSize)
+
+        time.sleep(1)
+
         
         time.sleep(2)
 
-        print("HEALTH STATUS: \n {0}'s health is {1} \n{2}'s health is {3}".format(firstPname, firstP.hp,secondPname, secondP.hp))
+def healthStatus(firstP, secondP):
+    firstP_healthBar, firstP_percent = do_health(firstP.hp, firstP.defaultHP)
+    secondP_healthBar, secondP_percent = do_health(secondP.hp, secondP.defaultHP)
+    print("########################################## HEALTH STATUS ##########################################\n#                          {0}'s health is {1} - {2} {3}\n#                          {4}'s health is {5} - {6} {7}\n###################################################################################################".format(firstP.title, firstP.hp, firstP_healthBar, firstP_percent, secondP.title, secondP.hp, secondP_healthBar, secondP_percent))
 
-        break
+def turnDef(Char):
+    print("------------------{0}'s TURN------------------".format(Char.title))
+    return Char
+
+def setTurnOrder(Char1, Char2):
+    Attacker = []
+    Defencer = []
+    for _ in range(100):
+        Attacker.append(Char1)
+        Attacker.append(Char2)
+    for _ in range(100):
+        Defencer.append(Char2)
+        Defencer.append(Char1)
+    return Attacker, Defencer
 
 if __name__ == "__main__":
     main()
