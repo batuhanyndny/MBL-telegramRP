b365709e9891108df845a046b25de91212d635b5
batuhan <batuhanyndny@gmail.com>
2019-08-09 11:01:38 +0300

    major fixes in classes and some code cleaning

--- classes.py
+++ classes.py
@@ -1,3 +1,13 @@
+'''
+TODO
+-Moves
+    
+
+-CHARS
+
+
+'''
+
 import random
 import time
 
@@ -46,28 +56,55 @@ class Moves:
     def invalidMove():
         return 
 
-    def amguard(Char):
-        Char.assholeArmor += 80
-        print('{0} HAS GAINED 80 ASSHOLE ARMOR'.format(Char.title))
+    def amguard(attacker):#legendary
+        attacker.assholeArmor += 80
+        print('{0} HAS GAINED 80 ASSHOLE ARMOR'.format(attacker.title))
         time.sleep(2)
 
 
-    def quadroDick(Char):
-        print("{0}'s dick size increased to {1} cm NOW HAS x4 DAMAGE".format(Char.title, Char.dickSize*4))
-        Char.dickSize = Char.dickSize*4
+    def quadroDick(attacker, defencer):#legendary
+        print("{0}'s dick size increased to {1} cm NOW HAS x4 DAMAGE".format(attacker.title, attacker.dickSize*4))
+        attacker.dickSize = attacker.dickSize*4
+        attacker.attack(defencer, attacker.dickSize)
+        resetDS(attacker)
         time.sleep(2)
 
-    def kucultucu(Char):
-        Char.dickSize = 2
-        print("{0}'s dick size decrased to 2 cm DAMN NIGGA MICROPENIS".format(Char.title))
+    def kucultucu(defencer): #legendary
+        defencer.dickSize = 2
+        print("{0}'s dick size decrased to 2 cm DAMN NIGGA MICROPENIS".format(defencer.title))
         time.sleep(2)
 
-    def geciktirici(Char):
-        Char.dickSize += 2
-        print("{0} used geciktirici, his attack powered and hits twice".format(Char.title))
+    def geciktirici(attacker, defencer): #regular
+        print("{0} used geciktirici, his attack powered and hits twice".format(attacker.title))
+        attacker.dickSize += 2
+        attacker.attack(defencer, attacker.dickSize)
+        time.sleep(1)
+        attacker.attack(defencer, attacker.dickSize)
+        resetDS(attacker)
         time.sleep(2)
+
+    def dickSlap(attacker, defencer): #regular 
+        print("{0} is getting ready for slappin yo ass".format(attacker.title))
+        attacker.dickSize += 3
+        attacker.attack(defencer, attacker.dickSize)
+        resetDS(attacker)
+        
+
+    def spankDaddy(attacker, defencer): #regular 
+        print("WOOHOO THAT'S HOT. {0} GONNA SPANK YOU HARD".format(attacker.title))
+        attacker.dickSize += 4
+        attacker.attack(defencer, attacker.dickSize)
+        resetDS(attacker)
+        
+
+    def assholeVacuum(attacker, defencer): #legendary 
+        print("DAMN LOOK AT THAT VACUUM POWER ! ALL OF THIS ONLY WITH MOUTH")
+        attacker.dickSize += 48
+        attacker.attack(defencer, attacker.dickSize)
+        resetDS(attacker)
+        
     
-    def ReverseCard(attacker, defencer, moveList, moveIndex):
+    def ReverseCard(attacker, defencer, moveList, moveIndex): #legendary
         move = moveList[moveIndex]
         if move == 'amguard':
             print("REVERSE CARD!\n{0}'s amguard taken back lol".format(defencer.title))
@@ -76,47 +113,96 @@ class Moves:
         elif move == 'quadroDick':
             print("REVERSE CARD!\nGOTCHAA, {0}'s DICK DID NOTHINGG".format(defencer.title))
             Moves.quadroDick(attacker)
-            attacker.attack(defencer, attacker.dickSize)
-            attacker.dickSize = attacker.defaultDickSize
-            if attacker.assholeArmor > 0:
-                attacker.assholeArmor += defencer.dickSize*4
-            else:
-                if (attacker.hp + defencer.dickSize*4) > attacker.defaultHP:
-                    extraArmor = defencer.dickSize*4 - (attacker.defaultHP - attacker.hp)
-                    attacker.hp = attacker.defaultHP
-                    attacker.assholeArmor += extraArmor
-                else:
-                    attacker.hp += defencer.dickSize*4
+            armorHpRegulizer(attacker,defencer.dickSize*4)
         elif move == 'kucultucu':
             Moves.kucultucu(defencer)
             print("REVERSE CARD!\nNIGGA {0} PUT THE KUCULTUCU TO {1}'s DICK".format(attacker.title, defencer.title))
         elif move == 'geciktirici':
             print("REVERSE CARD!\nNOPE {0} USED BETTER GECIKTIRICI".format(attacker.title))
             Moves.geciktirici(attacker)
-            attacker.attack(defencer, attacker.dickSize)
-            time.sleep(1)
-            attacker.attack(defencer, attacker.dickSize)
-            attacker.dickSize = attacker.defaultDickSize
-            if attacker.assholeArmor > 0:
-                attacker.assholeArmor += defencer.dickSize+defencer.dickSize
-            else:
-                attacker.hp += defencer.dickSize+defencer.dickSize
+            armorHpRegulizer(attacker,defencer.dickSize+defencer.dickSize)
         elif move == 'ReverseCard':
             newMoveIndex = moveIndex - 1
             Moves.ReverseCard(attacker, defencer, moveList, newMoveIndex)
         elif move == 'invalidMove':
             print('Man last move was invalid you wasted your reverseCard')
+        elif move == 'dickSlap':
+            print('REVERSE CARD !! THATS HOW YOU SLAP')
+            Moves.dickSlap(attacker, defencer)
+            armorHpRegulizer(attacker,defencer.dickSize+3)
+        elif move == 'spankDaddy':
+            print('REVERSE CARD !! RESPANK !')
+            Moves.spankDaddy(attacker, defencer)
+            armorHpRegulizer(attacker,defencer.dickSize+4)
+        elif move == 'assholeVacuum':
+            print('REVERSE CARD !! THATS HOW YOU VACCUM BITCHH')
+            Moves.assholeVacuum(attacker, defencer)
+            armorHpRegulizer(attacker, defencer.dickSize+48)
 
 
+def armorHpRegulizer(attacker, damage):
+    if attacker.assholeArmor > 0:
+        attacker.assholeArmor += damage
+    else:
+        if (attacker.hp + damage) > attacker.defaultHP:
+            extraArmor = damage - (attacker.defaultHP - attacker.hp)
+            attacker.hp = attacker.defaultHP
+            attacker.assholeArmor += extraArmor
+        else:
+            attacker.hp += damage
 
+def resetDS(attacker):
+    attacker.dickSize = attacker.defaultDickSize
 
 def currentCharList():
     CharList = {
-        'buyukbabaMax' : ['Buyukbaba MAX', 14, 'BUYUKBABA MAX' ],
+        'buyukbabaMax' : ['BuyukbabaMAX', 14, 'BUYUKBABA MAX' ],
         'emanetoglu' : ['EMANETOGLU', 12, "GIRESUNLU ASIRET VARISI"]
     }
 
     return CharList
+
+def currentMoveList():
+    MoveList = {
+        'regular' : ['geciktirici', 'dickSlap', 'spankDaddy'],
+        'legendary' : ['amguard', 'quadroDick', 'kucultucu', 'assholeVacuum', 'ReverseCard']
+
+    }
+    return MoveList
+
+def setAvaliableMoves(roundNumber, regularMoves, legendaryMoves):
+    avaliableMoves = {}
+    if roundNumber < 4:
+        indx1, indx2, indx3 = random.sample(range(0,3),3)
+        indx4 = random.randint(0,4)
+        avaliableMoves.update({1:regularMoves[indx1]})
+        avaliableMoves.update({2:regularMoves[indx2]})
+        avaliableMoves.update({3:regularMoves[indx3]})
+        avaliableMoves.update({4:legendaryMoves[indx4]})
+    else:
+        indx1, indx2 = random.sample(range(0,3),2)
+        indx3, indx4 = random.sample(range(0,5),2)
+        avaliableMoves.update({1:regularMoves[indx1]})
+        avaliableMoves.update({2:regularMoves[indx2]})
+        avaliableMoves.update({3:legendaryMoves[indx3]})
+        avaliableMoves.update({4:legendaryMoves[indx4]})
+
+    return avaliableMoves
+
+        
+
+def AnnounceRoundNumber(number):
+    roundNum = 0
+    if number % 1 == 0.0 or number % 1 == 0:
+        roundNum = int(number)
+        print("##################################################################################### ROUND {0} ####################################################################################".format(roundNum))
+    else:
+        roundNum = int(number)
+    return roundNum
+
+
+
+
 
 # def chooseRandomChar():
 #     fp = random.randint(0,1)
--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def main():
 
     moveList = []
 
+    defMoves = currentMoveList()
+
+    roundCounter = 1
+
     time.sleep(2)
 
     print('{0} plays as {1}'.format(firstPname, firstP.title))
@@ -44,6 +48,10 @@ def main():
         attacker = Attacker.__next__()[1]
 
         defencer = Defencer.__next__()[1]
+
+        roundNumber = AnnounceRoundNumber(roundCounter)
+        time.sleep(1)
+
         print('\n')
         turnDef(attacker)
         print('\n')
@@ -54,52 +62,66 @@ def main():
         time.sleep(2)
 
         print('\n')
+        
+        movesDict = setAvaliableMoves(roundNumber, defMoves['regular'], defMoves['legendary'])
 
-        print("AVALIABLE MOVES : \namguard --1 \nquadroDick --2 \nkucultucu --3 \ngeciktirici --4\nReverseCard --5")
+        moveNames, moveValues = list(movesDict.values()) , list(movesDict.keys())
+
+        print("AVALIABLE MOVES : \n{0} --{1} \n{2} --{3} \n{4} --{5} \n{6} --{7}".format(moveNames[0], moveValues[0], moveNames[1], moveValues[1], moveNames[2], moveValues[2], moveNames[3], moveValues[3]))
         
         print('\n')
 
-        move = input("Select by typing your moves number ->  ")
-        
-        if move == "1":
-            if attacker.assholeArmor > 100:
-                print("NIGGA YOU CANT WEAR 2 AMGUARDS AT THE SAME TIME, DAMN")
-                moveList.append(Moves.invalidMove())
-            else:
-                Moves.amguard(attacker)
-                moveList.append('amguard')
+        moveValue = input("Select by typing your moves number ->  ")
 
-        elif move == '2':
-            Moves.quadroDick(attacker)
-            attacker.attack(defencer, attacker.dickSize)
-            attacker.dickSize = attacker.defaultDickSize
+        try:
+            move = movesDict[int(moveValue)]
+        except:
+            print("YOU DIDN'T ENTERED A VALID MOVE. YOU FOOL, WASTED YOUR TURN")
+            move = 'invalid'
+            moveList.append('invalidMove')
+
+
+        if move == "amguard":
+            Moves.amguard(attacker)
+            moveList.append('amguard')
+
+        elif move == 'quadroDick':
+            Moves.quadroDick(attacker, defencer)
             moveList.append('quadroDick')
 
-        elif move == '3':
+        elif move == 'kucultucu':
             Moves.kucultucu(defencer)
             moveList.append('kucultucu')
 
-        elif move == '4':
-            Moves.geciktirici(attacker)
-            attacker.attack(defencer, attacker.dickSize)
-            time.sleep(1)
-            attacker.attack(defencer, attacker.dickSize)
-            attacker.dickSize = attacker.defaultDickSize
+        elif move == 'geciktirici':
+            Moves.geciktirici(attacker, defencer)
             moveList.append('geciktirici')
-
-        elif move == '5':
+            
+        elif move == 'ReverseCard':
             Moves.ReverseCard(attacker, defencer, moveList, -1)
             moveList.append('ReverseCard')
 
-        else:
-            print("YOU DIDN'T ENTERED A VALID MOVE. YOU FOOL, WASTED YOUR TURN")
-            moveList.append('invalidMove')
+        elif move == 'dickSlap':
+            Moves.dickSlap(attacker, defencer)
+            moveList.append('dickSlap')
+
+        elif move == 'spankDaddy':
+            Moves.spankDaddy(attacker, defencer)
+            moveList.append('spankDaddy')
+
+        elif move == 'assholeVacuum':
+            Moves.assholeVacuum(attacker, defencer)
+            moveList.append('assholeVacuum')
+            
 
         
-        time.sleep(2)
+        time.sleep(1)
+
+        roundCounter += 0.5
+
     
     print("\n"*3)
-    print('KO')
+    print('SWINGING SEMEN SHOT')
     print("\n"*3)
 
     if firstP.hp <= 0:
